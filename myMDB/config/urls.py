22bc7ff0fa2bea9347b23ddb80edb41d10cf5ff7
Obdulia Lopez <olopez@sdfutures.org>
2019-06-19 14:48:53 -0700

    added import

--- myMDB/config/urls.py
+++ myMDB/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url
 import core.urls
 import user.urls
 
